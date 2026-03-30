a0c56ee0935fc81684160070d69b2256685a29dc
Afrika Gabriel <shikukugabriel06@gmail.com>
2026-03-30 16:55:04 +0300

    managed loading states

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import { useState,useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isLoading, setIsLoading] = useState(true);
   const [availablePlaces, setAvailablePlaces]=useState([]);
  useEffect(()=>{
   const fetchPlaces = async () => {
+    setIsLoading(true);
 
     const res = await fetch("http://localhost:3000/places");
     const data = await res.json();
     setAvailablePlaces(data.places);
+    setIsLoading(false);
   }
  fetchPlaces();
  },[]);
@@ -19,6 +22,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      LoadingText ="Loading available Places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
